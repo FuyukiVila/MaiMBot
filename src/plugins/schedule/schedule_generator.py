63cb8edbc71e3d72544fe62b4742095b00b2e3fc
FuyukiVila <1642421711@qq.com>
2025-04-16 19:48:20 +0800

    restore

--- src/plugins/schedule/schedule_generator.py
+++ src/plugins/schedule/schedule_generator.py
@@ -33,13 +33,13 @@ class ScheduleGenerator:
         self.llm_scheduler_all = LLMRequest(
             model=global_config.llm_reasoning,
             temperature=global_config.SCHEDULE_TEMPERATURE + 0.3,
-            max_tokens=global_config.max_response_length + 10000,
+            max_tokens=7000,
             request_type="schedule",
         )
         self.llm_scheduler_doing = LLMRequest(
             model=global_config.llm_normal,
             temperature=global_config.SCHEDULE_TEMPERATURE,
-            max_tokens=global_config.max_response_length,
+            max_tokens=2048,
             request_type="schedule",
         )
 
