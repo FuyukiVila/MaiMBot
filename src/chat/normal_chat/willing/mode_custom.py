bdecfd5edd62ba0c2aec686573442f89e54a8d32
FuyukiVila <1642421711@qq.com>
2025-05-17 01:00:50 +0800

    restore

--- src/chat/normal_chat/willing/mode_custom.py
+++ src/chat/normal_chat/willing/mode_custom.py
@@ -1,15 +1,24 @@
-from .mode_dynamic import DynamicWillingManager
+from .willing_manager import BaseWillingManager
 
 
-class CustomWillingManager(DynamicWillingManager):
-    async def get_reply_probability(self, message_id):
-        willing_info = self.ongoing_messages[message_id]
-        group_info = willing_info.message.message_info.group_info
-        if not group_info:
-            if willing_info.is_mentioned_bot:
-                return 1.0
-            elif not willing_info.is_emoji:
-                return 0.75
-            else:
-                return 0.1
-        return await super().get_reply_probability(message_id)
+class CustomWillingManager(BaseWillingManager):
+    async def async_task_starter(self) -> None:
+        pass
+
+    async def before_generate_reply_handle(self, message_id: str):
+        pass
+
+    async def after_generate_reply_handle(self, message_id: str):
+        pass
+
+    async def not_reply_handle(self, message_id: str):
+        pass
+
+    async def get_reply_probability(self, message_id: str):
+        pass
+
+    async def bombing_buffer_message_handle(self, message_id: str):
+        pass
+
+    def __init__(self):
+        super().__init__()
